98274c586ba4e874c13e1c5a862f4050f422d40c
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-06-07 16:09:46 -0300

    lectura de archivos

--- TPN8/Program.cs
+++ TPN8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+//Console.WriteLine("Ingrese una direccion: ");
+//string ingresoDireccion = Console.ReadLine();
+
+string direccionArchivos = @"C:\Users\santiago\Desktop\Facultad\Taller_de_lenguaje_1\TPN8\tp08-2022-SantiagoECastillo\Archivos_prueba";
+DirectoryInfo direccion = new DirectoryInfo(direccionArchivos);
+Console.WriteLine("Los archivos del direcctorio ingresados son:");
+foreach (var archivo in direccion.GetFiles())
+{
+    Console.WriteLine(archivo.Name);    
+}
